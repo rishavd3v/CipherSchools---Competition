3ba886bf9bef001c1af086e69f46a10c1b8cff77
Rishav <rishavraj4273@gmail.com>
2023-08-22 19:23:23 +0530

    modified

--- Day 1/Day1.cpp
+++ Day 1/Day1.cpp
@@ -3,13 +3,16 @@
 using namespace std;
 
 int main(){
-
-    int arr[] = {1,8,6,2,5,4,8,3,7};
-    int len = sizeof(arr)/sizeof(arr[0]);
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
     int max_area = 0;
 
-    for(int i=0;i<len;i++){
-        for (int j = i+1; j<len; j++){
+    for(int i=0;i<n;i++){
+        for (int j = i+1; j<n; j++){
             int height = min(arr[i],arr[j]);
             int area = (j-i)*height;
             if(max_area<=area){
