532a173bc5475082003a73856fb69ea39e0063df
Rishav <rishavraj4273@gmail.com>
2023-08-22 19:01:39 +0530

    Day - 1 solution

--- Day 1/Day1.cpp
+++ Day 1/Day1.cpp
@@ -6,18 +6,17 @@ int main(){
 
     int arr[] = {1,8,6,2,5,4,8,3,7};
     int len = sizeof(arr)/sizeof(arr[0]);
-    int max = 0;
-    int area = 0;
+    int max_area = 0;
 
     for(int i=0;i<len;i++){
         for (int j = i+1; j<len; j++){
             int height = min(arr[i],arr[j]);
-            area = (j-i)*height;
-            if(max<=area){
-                max = area;
+            int area = (j-i)*height;
+            if(max_area<=area){
+                max_area = area;
             }
         }
     } 
-    cout<<max;
+    cout<<max_area;
     return 0;
 }
